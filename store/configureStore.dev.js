09c09708583b7a115a9fa96b1833d98c406a21d9
yofine <yofineliu@gmail.com>
2016-05-18 13:54:11 +0800

    123

--- store/configureStore.dev.js
+++ store/configureStore.dev.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import Immutable from 'immutable'
 import thunk from 'redux-thunk'
-import createLogger from 'redux-logger'
 import rootReducer from '../reducers'
 import { routerMiddleware } from 'react-router-redux'
 import { browserHistory } from 'react-router'
@@ -14,19 +13,7 @@ export default function configureStore(initialState) {
     rootReducer,
     initialState,
     compose(
-      applyMiddleware(thunk, middleware, createLogger({
-        stateTransformer: (state) => {
-          var newState = {};
-          for (var i of Object.keys(state)) {
-            if (Immutable.Iterable.isIterable(state[i])) {
-              newState[i] = state[i].toJS();
-            } else {
-              newState[i] = state[i];
-            }
-          };
-          return newState;
-        }
-      })),
+      applyMiddleware(thunk, middleware),
       DevTools.instrument()
     )
   )

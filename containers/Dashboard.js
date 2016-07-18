b86c772862cdccfa4df756222f705dd9535791c8
Administrator <admin@example.com>
2016-07-18 11:28:09 +0800

    Dashboard Container 注释

--- containers/Dashboard.js
+++ containers/Dashboard.js
@@ -4,14 +4,47 @@ import { bindActionCreators } from 'redux'
 
 class Dashboard extends Component {
 
+  // 初始化
   constructor(props, context) {
     super(props, context)
   }
 
+  // 组件待挂载
   componentWillMount() {
 
   }
 
+  // 组件已挂载
+  componentDidMount() {
+
+  }
+
+  // 组件接收到新的参数
+  componentWillReceiveProps() {
+
+  }
+
+  // 组件是否更新
+  shouldComponentUpdate() {
+
+  }
+
+  // 组件待更新
+  componentWillUpdate() {
+
+  }
+
+  // 组件已更新
+  componentDidUpdate() {
+
+  }
+
+  // 组件待卸载
+  componentWillUnmount() {
+
+  }
+
+  // 渲染
   render() {
     const style = {
       height: document.documentElement.clientHeight,
@@ -28,12 +61,15 @@ class Dashboard extends Component {
   }
 }
 
+// 绑定 Store 中的状态到当前 container
 const mapStateToProps = (state) => ({
 
 })
 
+// 绑定 Action 到当前 container
 const mapDispatchToProps = (dispatch) => ({
 
 })
 
+// 绑定
 export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
